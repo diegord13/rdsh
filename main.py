76df529ef20ee796ed429819780ff8e922c1a110
BDG-DAROBAYO <diego.robayo@ittic.com.co>
2022-06-02 15:46:28 -0500

    get info

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
-
+import numpy as np
+from itertools import chain
+import re
 
 def takeExcel():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -28,15 +30,33 @@ def takeExcel():
                             8:'Amount',
                             9:'Balance'
                             })
-    # view the data
-    df_filtered = df[(df.Transaction_Type =='Journal Entry') | (df.Transaction_Type =='Bill') | (df.Transaction_Type =='Vendor Credit')]
-    df_filtered = df_filtered.drop(['0', 'Class','Balance'], axis=1)
+
+    df = df.drop(['0', 'Class', 'Balance'], axis=1)
+    df = df[(df.Transaction_Type == 'Journal Entry') | (df.Transaction_Type == 'Bill') | (df.Transaction_Type == 'Vendor Credit')]
+
+    #df_filtered['Date'] = pd.to_datetime(df_filtered['Date'], format='%d/%m/%Y', errors='coerce')
+    #df_filtered.dropna(subset=["Date"], inplace=True)
+    #df = df_filtered["Memo/Description"].str.split('\n').explode('Memo/Description')
+
+    def repeater(s):
+        return list(chain.from_iterable(s.str.split('\n')))
 
 
-    print(df_filtered)
+    lens = df['Memo/Description'].str.split('\n').map(len)
 
-    df_filtered.to_csv('sheet_name', header=True, index=False)
-
+    # create new dataframe, repeating or chaining as appropriate
+    res = pd.DataFrame({'Date': np.repeat(df['Date'], lens),
+                        'Transaction_Type': np.repeat(df['Transaction_Type'], lens),
+                        'Num': np.repeat(df['Num'],lens),
+                        'Name': np.repeat(df['Name'],lens),
+                        'Memo/Description': repeater(df['Memo/Description']),
+                        'Account': np.repeat(df['Account'],lens),
+                        'Amount': np.repeat(df['Amount'],lens)
+                        })
+    res['Amount 1'] = res['Memo/Description'].str.extract("([\d,.]+)[^\d,.]*$")
+    res['Title match'] = res['Memo/Description'].str.extract("(?i)\b")
+    print(res.shape)
+    res.to_csv('sheet_name', header=True, index=False)
 
 
 if __name__ == '__main__':
